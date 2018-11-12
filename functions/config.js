39c499dc2d41e36b463857e57068aba480b48978
arut klaewkasetkorn <60090002@kmitl.ac.th>
2018-11-12 11:02:28 +0700

    added postback handler

--- functions/config.js
+++ functions/config.js
@@ -4,6 +4,10 @@ const config = {
     channelAccessToken: functions.config().line.channel_access_token,
     channelSecret: functions.config().line.channel_secret,
     channelId: functions.config().line.channel_id,
+    mainMenuId: functions.config().line.main_menu_id,
+    airControllerMenuId: functions.config().line.air_controller_menu_id,
+    lightControllerMenuId: functions.config().line.light_controller_menu_id,
+    roomControllerMenuId:  functions.config().line.room_controller_menu_id
 }
 
 module.exports = config
--- functions/index.js
+++ functions/index.js
@@ -2,8 +2,12 @@ const functions = require('firebase-functions')
 const admin = require('firebase-admin')
 const config = require('./config.js')
 const line = require('@line/bot-sdk')
+const url = require('url')
 
 admin.initializeApp(functions.config().firebase)
+
+const db = admin.database()
+const ref = db.ref()
 
 const client = new line.Client(config)
 
@@ -17,9 +21,9 @@ exports.webhook = functions.https.onRequest((req, res) => {
             // check verify webhook event
             if (event.replyToken === '00000000000000000000000000000000' ||
                 event.replyToken === 'ffffffffffffffffffffffffffffffff') {
-                return;
+                return null;
             }
-            handleEvent(event)
+            return handleEvent(event)
         }))
         .then((result) => res.json(result))
         .catch((err) => {
@@ -30,8 +34,137 @@ exports.webhook = functions.https.onRequest((req, res) => {
 })
 
 function handleEvent(event) {
-    const echo = { type: 'text', text: event.message.text };
-    client.replyMessage(event.replyToken, echo);
+    switch (event.type) {
+        case 'message': {
+            const echo = { type: 'text', text: event.message.text };
+            return client.replyMessage(event.replyToken, echo);
+        }
+        case 'postback': {
+            return handlePostback(event)
+        }
+        default:
+            return client.replyMessage(event.replyToken, { type: 'text', text: event.message.text });
+    }
 }
+
+function handlePostback(event) {
+    var data = url.parse('?' + event.postback.data, true).query
+    console.log(data)
+    const userId = event.source.userId
+    console.log(userId)
+    switch (data.action) {
+        case 'RICHMENU':
+            return handleNavigation(data.menu, userId)
+        case 'DOOR_UNLOCK':
+            return handleDoorUnlock(userId)
+        case 'AIR':
+            return handleAirController(data, userId)
+        case 'LIGHT':
+            return null
+    }
+    return null
+}
+
+async function handleAirController(data, userId) {
+    const userRef = db.ref('users')
+
+    const userSnapshot = await userRef.child(userId).once("value")
+    const roomKey = userSnapshot.val().roomKey
+    const roomRef = db.ref('rooms').child(roomKey)
+    console.log(data)
+    console.log(data.status)
+    console.log(data.temp)
+    if(data.status !== undefined) {
+        if(data.status === "ON")await roomRef.update({ airStatus: true })
+        else await roomRef.update({ airStatus: false })
+    } else if(data.temp !== undefined) {
+        // const currentTemp = await roomRef.once("value").temp
+        if(data.temp === "UP")await roomRef.child('temp').transaction(currentTemp => currentTemp + 1)
+        else await roomRef.child('temp').transaction(currentTemp => currentTemp - 1)
+    } 
+    const roomSnapshot = await roomRef.once("value")
+    const temp = roomSnapshot.val().temp
+    const status = roomSnapshot.val().airStatus
+    const msg = {
+        type: "text",
+        text: `Temperature: ${temp}\nStatus: ${status}`
+    }
+
+    return client.pushMessage(userId, msg)
+}
+
+async function handleDoorUnlock(userId) {
+    await client.pushMessage(userId, { type: 'text', text: 'Unlocking ...' })
+    var userRef = db.ref('users')
+
+    const userSnapshot = await userRef.child(userId).once("value")
+    console.log(userSnapshot.val())
+    if (!userSnapshot.exists()) {
+        var profile = await client.getProfile(userId)
+        var roomRef = await db.ref('rooms').push({
+            airStatus: false,
+            bathroomLightStatus: false,
+            bedroomLightStatus: false,
+            doorStatus: true,
+            temp: 25,
+            user: userId
+        })
+        profile.roomKey = roomRef.key
+        userRef.child(userId).update(profile)
+        await client.pushMessage(userId, { type: "text", text: "The door is unlocked" })
+    } else {
+        var roomKey = await userSnapshot.val().roomKey
+        db.ref('rooms').child(roomKey).update({
+            doorStatus: true
+        })
+        await client.pushMessage(userId, { type: "text", text: "The door is unlocked" })
+    }
+    return "done"
+}
+
+function handleNavigation(menu, userId) {
+    switch (menu) {
+        case 'MAIN':
+            return client.linkRichMenuToUser(userId, config.mainMenuId)
+        case 'ROOM_CONTROLLER':
+            return client.linkRichMenuToUser(userId, config.roomControllerMenuId)
+        case 'LIGHT_CONTROLLER':
+            return client.linkRichMenuToUser(userId, config.lightControllerMenuId)
+        case 'AIR_CONTROLLER':
+            return client.linkRichMenuToUser(userId, config.airControllerMenuId)
+        default:
+            return client.linkRichMenuToUser(userId, config.mainMenuId)
+    }
+}
+
+exports.richMenuList = functions.https.onRequest((req, res) => {
+    client.getRichMenuList()
+        .then((result) => res.send(result))
+        .catch((err) => {
+            console.error(err)
+            res.status(404).end()
+        })
+})
+
+exports.unlockSuccess = functions.database.ref('rooms/{roomKey}/doorStatus').onUpdate(async (change, context) => {
+    if (change.before.exists()) {
+        return null;
+    }
+    // Exit when the data is deleted.
+    if (!change.after.exists()) {
+        return null;
+    }
+    const doorStatus = change.after.val();
+    const roomKey = context.params.roomKey
+    var dataSnapshot = await db.ref('room').child(roomKey).once("value")
+    const userId = dataSnapshot.val().userId
+    console.log("UserId: " + userId)
+    console.log("DoorStatus: " + doorStatus)
+    console.log("RoomKey: " + roomKey)
+
+    await client.pushMessage(userId, {type: "text", text: `Door Status: Hello World`})
+    return Promise.resolve("Done")
+})
+
 
 
